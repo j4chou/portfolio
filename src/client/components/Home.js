a8e096179236aaacb9e4b596a18cf89d51c761be
Jessica Chou <j4chou@gmail.com>
2017-03-18 18:23:40 -0700

    Remove console log from Home component

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -2,11 +2,9 @@ import React from 'react';
 import Header from './Header';
 
 const Home = ({ children }) => {
-  console.log('children', children)
   return (
     <div className="container">
       <Header />
-      {children}
     </div>
   );
 };
