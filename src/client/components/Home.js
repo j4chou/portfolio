a10d16856bd573809c4abc7139e3f877c55ad141
Jessica Chou <j4chou@gmail.com>
2017-03-24 22:53:33 -0700

    Stuck on getting image paths to show

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header/index';
+import Icons from './Icons/index';
 
 const Home = ({ children }) => {
   return (
     <div className="container">
       <Header />
+      <Icons />
     </div>
   );
 };
--- src/client/components/Icons/index.js
+++ src/client/components/Icons/index.js
@@ -1,14 +1,35 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router';
 import './styles.scss';
 
-class Icons extends Component {
-  render() {
-    return (
-      <div className="name">
-       Jessica Chou
-      </div>
-    );
-  }
+const Icons = () => {
+  const links = [
+    { name: 'github',
+      path: 'http://github.com/j4chou', 
+      src: '/public/assets/github.png'
+     },
+    { name: 'linkedin',
+      path: 'http://www.linkedin.com/in/jessicachou1/', 
+      src: '/public/assets/linkedin.png' 
+    }
+  ];
+
+  return (
+    <div className="icon-navbar">
+      <ul>
+      {
+        links.map( link => {
+          let linkClass = 'icon-nav-link';
+          return (
+            <Link to={link.path} key={link.name}>
+              <img className={linkClass} src={link.src} alt={link.name} />
+            </Link>
+          );
+        })
+      }
+      </ul>
+    </div>
+  );
 }
 
 export default Icons;
