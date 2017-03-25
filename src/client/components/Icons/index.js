ba5824ba692e554436c3dc8b63daffa5fbad8c87
Jessica Chou <j4chou@gmail.com>
2017-03-24 22:06:35 -0700

    Create icons component. Added assets

---
+++ src/client/components/Icons/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.scss';
+
+class Icons extends Component {
+  render() {
+    return (
+      <div className="name">
+       Jessica Chou
+      </div>
+    );
+  }
+}
+
+export default Icons;
