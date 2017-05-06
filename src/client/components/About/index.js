00aec22d41522a27b3c95542165a3ae0b76ed67a
Jessica Chou <j4chou@gmail.com>
2017-05-06 16:15:12 -0700

    Style about page

--- src/client/components/About/index.js
+++ src/client/components/About/index.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+
 import './../../styles/base.scss';
+import './styles.scss';
 
 class About extends Component {
   render() {
     return (
-      <div className="content">
-      about here
+      <div className="about content">
+        <div className="picture">
+        </div>
+        <div className="about-me">
+          <p>About</p>
+        </div>
       </div>
     );
   }
