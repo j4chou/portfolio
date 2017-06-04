6eabd0a838f82794fc795067f0a6212b89acaf59
Jessica Chou <j4chou@gmail.com>
2017-06-04 01:56:58 -0700

    Add facts

--- src/client/components/About/index.js
+++ src/client/components/About/index.js
@@ -13,19 +13,25 @@ class About extends Component {
         </div>
         <div className="about-me">
           <ul>
-            <li>Born and raised in Los Angeles, CA</li>
-            <li>Graduate of UC San Diego Class of 2015</li>
-            <li>Motivated web developer with a love of challenges</li>
-            <li>Some activities I do for fun:
-                <ul>
-                  <li>Searching for top-rated books to read (and reading them)</li>
-                  <li>Cooking recipes with a reasonable list of ingredients</li>
-                  <li>Rewatching Studio Ghibli films</li>
-                  <li>Rock climbing</li>
-                  <li>Planning food adventures with friends & family</li>
-                </ul>
-            </li>
-            <li>I have a certified typing speed of 101 WPM with 100% accuracy (5 minute test)</li>
+            <h2>About me</h2>
+              <ul>
+              <li>Born and raised in Los Angeles, CA</li>
+              <li>Graduate of UC San Diego Class of 2015</li>
+              <li>Passionate JavaScript developer who loves a challenge</li>
+              </ul>
+            <h2>What are my hobbies?</h2>
+              <ul>
+                <li>Searching for top-rated books to read - and reading them</li>
+                <li>Cooking recipes with a reasonable list of ingredients</li>
+                <li>Rewatching Studio Ghibli films</li>
+                <li>Rock climbing (not real rocks, though)</li>
+                <li>Planning food adventures with friends & family</li>
+              </ul>
+            <h2>Interesting(?) Facts</h2>
+              <ul>
+                <li>I have a certified typing speed of 101 WPM with 100% accuracy (5 minute test)</li>
+                <li>I was once present at a restaurant when it was robbed at gunpoint</li>
+              </ul>
           </ul>
         </div>
       </div>
