28734d7721d8c9bef2870b5c50cf0844c878b8ef
Jessica Chou <j4chou@gmail.com>
2017-06-04 18:16:58 -0700

    More facts and color changes.

--- src/client/components/About/index.js
+++ src/client/components/About/index.js
@@ -16,21 +16,25 @@ class About extends Component {
             <h2>About me</h2>
               <ul>
               <li>Born and raised in Los Angeles, CA</li>
+              <li>My parents immigrated from Vietnam and Taiwan</li>
               <li>Graduate of UC San Diego Class of 2015</li>
               <li>Passionate JavaScript developer who loves a challenge</li>
+              <li>I enjoy ingesting copious amounts of caffeine via tea and coffee</li>
               </ul>
             <h2>What are my hobbies?</h2>
               <ul>
                 <li>Searching for top-rated books to read - and reading them</li>
                 <li>Cooking recipes with a reasonable list of ingredients</li>
-                <li>Rewatching Studio Ghibli films</li>
-                <li>Rock climbing (not real rocks, though)</li>
-                <li>Planning food adventures with friends & family</li>
+                <li>Playing different types of games (console, board, card, etc.)</li>
+                <li>Rock climbing at a gym</li>
+                <li>Expanding my knowledge, whether it be learning new technologies or other topics like investing</li>
               </ul>
-            <h2>Interesting(?) Facts</h2>
+            <h2>Interesting Facts</h2>
               <ul>
+                <li>I <i>love</i> all-you-can-eat Korean barbecue/sushi/hotpot restaurants, and buffets</li>
+                <li>I was present at a local restaurant when it got robbed at gunpoint</li>
+                <li>I was the first in my (extended) family to graduate college</li>
                 <li>I have a certified typing speed of 101 WPM with 100% accuracy (5 minute test)</li>
-                <li>I was once present at a restaurant when it was robbed at gunpoint</li>
               </ul>
           </ul>
         </div>
